8fa7dd17de320b528666e99fae868bcd4b3175f5
nvrton <joshuanetherton1@gmail.com>
2022-11-08 10:41:42 +0000

    Create internetping.py

---
+++ internetping.py
@@ -0,0 +1,9 @@
+import urllib.request
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host) #Python 3.x
+        return True
+    except:
+        return False
+# test
+print( "connected" if connect() else "no internet!" )
